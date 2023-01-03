dfcf7ed1441ba2356baf3b76dbc782d44b8ecbf0
Mehmet Maden <yasamtr@gmail.com>
2023-01-03 16:54:21 +0300

    Locaters

---
+++ src/test/java/day03_Locaters/C04_Q.java
@@ -0,0 +1,12 @@
+package day03_Locaters;
+
+public class C04_Q {
+
+    //1- Go to  https://www.amazon.com/
+    //2- Search by typing "city bike" in the search box
+    // 3- Print the number of displayed results // solve it with Xpath
+    // 4- Click on the picture of the first product from the list.
+
+
+
+}
